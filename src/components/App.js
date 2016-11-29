e294305aa7154ff6345ed48bc356fcf9b17c71f0
Thom Allen <thomwork@gmail.com>
2016-11-29 23:43:59 +0000

    Add generation of random cell contents, dynamic cell array size

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Column from './Column';
 import styled from 'styled-components';
+import v4 from 'uuid';
 
 const Title = styled.h1`
   font-size: 1.5em;
+  font-family: sans-serif;
   text-align: center;
   color: palevioletred;
 `;
@@ -12,44 +14,48 @@ const StyledAppWrapper = styled.div`
   width: 45%;
   margin: 0 auto;
   text-align: center;
-`
+`;
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      cells: [
-        [
-          { content: 1 },
-          { content: 1 },
-          { content: 2 },
-          { content: 1 },
-          { content: 0 },
-        ],
-        [
-          { content: 2 },
-          { content: 2 },
-          { content: 1 },
-          { content: 0 },
-          { content: 2 },
-        ],
-        [
-          { content: 1 },
-          { content: 1 },
-          { content: 0 },
-          { content: 1 },
-          { content: 0 },
-        ],
-      ]
+      rows: 5,
+      cols: 3,
+      cells: [],
     };
   }
+  componentDidMount() {
+    this.generateCellsData();
+  }
+  generateCellsData() {
+    const { cols, rows } = this.state;
+    const cells = [];
+    for (let y = 0; y < cols; y++) {
+      cells.push([]);
+      for (let x = 0; x < rows; x++) {
+        cells[y].push({
+          content: this.generateCellContents(),
+          x: y,
+          y: x,
+        });
+      }
+    }
+    this.setState({
+      cells,
+    });
+  }
+  generateCellContents() {
+    return Math.round(Math.random() * 5);
+  }
   render() {
+    const { cells } = this.state;
     return (
       <StyledAppWrapper>
         <Title>match3</Title>
-        <Column cells={this.state.cells[0]} />
-        <Column cells={this.state.cells[1]} />
-        <Column cells={this.state.cells[2]} />
+        {cells.map((col) =>
+          <Column cells={col} key={v4()} />
+        )}
       </StyledAppWrapper>
     );
   }
