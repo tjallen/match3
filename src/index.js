bc84aae1d5501730e298761d88511f14acb8d57b
Thom Allen <thomwork@gmail.com>
2017-05-02 15:35:01 +0100

    Add logger middleware

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import './styles.css';
 import App from './components/App';
 // import match3 from './reducers';
 import cells from './reducers/cells';
 
-let store = createStore(cells);
+const logger = (store) => (next) => (action) => {
+  console.log('action fired', action);
+  next(action);
+};
 
-console.log('store =>', store.getState());
+const middleware = applyMiddleware(logger);
+
+const store = createStore(
+  cells,
+  middleware,
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 store.subscribe(() => {
-  document.body.innerText = store.getState();
-});
+  console.log('store changed', store.getState());
+})
+
+store.dispatch({type: 'E'})
 
 ReactDOM.render(
   <Provider store={store}>
